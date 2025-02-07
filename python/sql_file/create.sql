ad143faa51ff8c30b64afc40e8c1dea63394161e
NoCymer <61873313+NoCymer@users.noreply.github.com>
2025-02-07 08:22:50 +0100

    Add review object to DB

--- python/sql_file/create.sql
+++ python/sql_file/create.sql
@@ -1,4 +1,9 @@
 INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Silver Star', 'Montagne russe', 3, 1);
 INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Montagne 8', 'Montagne russe', 4, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Yoy', 'Montagne russe', 4, 0);
+
+INSERT INTO review (attraction_id, name, surname, comment, rating) VALUES (1, 'Ben', 'Dover', 'Ouaaaaouw so cool', 5);
+INSERT INTO review (attraction_id, name, surname, comment, rating) VALUES (1, 'Mike', 'Hawks', 'Bof', 3);
+INSERT INTO review (attraction_id, name, surname, comment, rating) VALUES (2, 'Vivite', 'Laire', 'Pas mal, vu mieux', 2);
 
 INSERT INTO users (name, password) VALUES ('toto', 'toto');
--- python/sql_file/init.sql
+++ python/sql_file/init.sql
@@ -1,4 +1,9 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS review;
 DROP TABLE IF EXISTS attraction;
+DROP TABLE IF EXISTS users;
+SET FOREIGN_KEY_CHECKS = 1;
+
 
 CREATE TABLE attraction (
     attraction_id int auto_increment,
@@ -9,7 +14,16 @@ CREATE TABLE attraction (
     visible bool default true
 );
 
-DROP TABLE IF EXISTS users;
+CREATE TABLE review (
+    review_id INT AUTO_INCREMENT,
+    attraction_id INT NOT NULL,
+    PRIMARY KEY(review_id),
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    comment VARCHAR(255) NOT NULL,
+    rating INT NOT NULL CHECK (rating BETWEEN 0 AND 5),
+    FOREIGN KEY (attraction_id) REFERENCES attraction(attraction_id) ON DELETE CASCADE
+);
 
 CREATE TABLE users (
     users_id int auto_increment,
